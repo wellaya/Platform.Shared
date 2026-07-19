fe2f648f3823221f9b93edc088483c62dfc3e590
Piyantha Weligamage <weligamagepriya@hotmail.com>
2026-07-19 11:33:04 +0800

    base db context

---
+++ src/Platform.Infrastructure/Persistence/BasePlatformDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Platform.Domain.Common;
+
+namespace Platform.Infrastructure.Persistence;
+
+public abstract class BasePlatformDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Ignore<BaseEvent>();
+        base.OnModelCreating(builder);
+    }
+}
